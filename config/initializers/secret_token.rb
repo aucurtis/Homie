86241d769f643b58c00d7d43f7ef8d380fed8332
Austin Curtis <austinjohncurtis@gmail.com>
2013-12-22 12:33:49 -0500

    First commite for homie

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Homie::Application.config.secret_key_base = '0a5de8916d434e0f89c8a48953f166a71e4ba537b1279842fe2eba6027dd8e526dd198fe648d54fbe008c2498f59341fcbcabe0be15eef1836454eaec9a68d27'
