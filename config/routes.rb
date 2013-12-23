3f65464aec0774628318c6f82f181eb9fd3bd897
Austin Curtis <austinjohncurtis@gmail.com>
2013-12-23 13:05:13 -0500

    linked up heroku to our new app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Homie::Application.routes.draw do
+  root "pages#home"
+  get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
